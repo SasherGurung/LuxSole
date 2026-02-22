1dd9e296010614c420d5a56f61bb408dc1b1cea7
Sasher Gurung <sashergrg602@gmail.com>
2026-02-22 21:49:22 +0545

    Add Dynamic Routing for Home UI

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,12 @@ import Image from "next/image";
 import arrowImg from "../public/assets/right-arrow.png";
 import introNikeImg from "../public/assets/intro-nike.png";
 import introAdidasImg from "../public/assets/intro-adidas.png";
-import kobe3ProtoImg from "../public/productsImg/kobe3proto.png";
-import nikeGTImg from "../public/productsImg/nikegt.png";
-import levron23EliteImg from "../public/productsImg/lebron23elite.png";
+import { products } from "../data/products"
 
 export default function Homepage() {
+
+  const featuredProducts = products.slice(0, 3)
+
   return (
     <section className="flex flex-col min-h-screen w-full">
       <div className="flex items-center w-full h-14 bg-amber-100 justify-around shadow-md">
@@ -64,53 +65,27 @@ export default function Homepage() {
       </div>
 
       <div className="grid grid-cols-3 gap-5 p-7">
-        <div className="rounded-2xl shadow-md aspect-square overflow-hidden hover:shadow-xl hover:scale-101 transition-all duration-300">
-          <div className="w-full h-2/3 overflow-hidden">
-            <Image
-              src={kobe3ProtoImg}
-              alt="Kobe 3 Proto"
-              className="w-full h-full object-cover hover:scale-105 transition-transform duration-300"
-            />
-          </div>
-          <div className="p-4">
-            <p className="text-red-600 font-semibold mt-1">Just In</p>
-            <p className="font-semibold text-[16px] mt-1">Kobe 3 Protro</p>
-            <p className="text-gray-500 mt-1">Basketball Shoes</p>
-            <p className="mt-1">$220</p>
-          </div>
-        </div>
-        <div className="rounded-2xl shadow-md aspect-square overflow-hidden hover:shadow-xl hover:scale-101 transition-all duration-300">
-          <div className="w-full h-2/3 overflow-hidden">
-            <Image
-              src={levron23EliteImg}
-              alt="Nike GT"
-              className="w-full h-full object-cover hover:scale-105 transition-transform duration-300"
-            />
-          </div>
-          <div className="p-4">
-            <p className="text-red-600 font-semibold mt-1">Best Seller</p>
-            <p className="font-semibold text-[16px] mt-1">LeBron XXIII Elite</p>
-            <p className="text-gray-500 mt-1">Basketball Shoes</p>
-            <p className="mt-1">$220</p>
-          </div>
-        </div>
-        <div className="rounded-2xl shadow-md aspect-square overflow-hidden hover:shadow-xl hover:scale-101 transition-all duration-300">
-          <div className="w-full h-2/3 overflow-hidden">
-            <Image
-              src={nikeGTImg}
-              alt="Nike GT"
-              className="w-full h-full object-cover hover:scale-105 transition-transform duration-300"
-            />
-          </div>
-          <div className="p-4">
-            <p className="text-red-600 font-semibold mt-1">Coming Soon</p>
-            <p className="font-semibold text-[16px] mt-1">
-              Nike G.T. Cut 4 CNY
-            </p>
-            <p className="text-gray-500 mt-1">Basketball Shoes</p>
-            <p className="mt-1">$220</p>
-          </div>
-        </div>
+        {featuredProducts.map((product) => (
+          <Link href={`/products/${product.id}`} key={product.id}>
+            <div 
+              className="rounded-2xl shadow-md aspect-square overflow-hidden hover:shadow-xl hover:scale-101 transition-all duration-300">
+              <div className="w-full h-2/3 overflow-hidden">
+                <Image
+                  src={product.img}
+                  alt={product.name}
+                  className="w-full h-full object-cover hover:scale-105 transition-transform duration-300"
+                />
+              </div>
+              <div className="p-4">
+                <p className="text-red-600 font-semibold mt-1">{product.tag}</p>
+                <p className="font-semibold text-[16px] mt-1">{product.name}</p>
+                <p className="text-gray-500 mt-1">{product.category}</p>
+                <p className="mt-1">$ {(product.priceCents / 100).toFixed(2)}</p>
+              </div>
+            </div>
+          </Link>
+        ))}
+
       </div>
     </section>
   );
